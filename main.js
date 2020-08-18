e8a95cbe75d11c532b309729adbc1baf0b851721
Your Name <61206200+Golamrabbani-ux@users.noreply.github.com>
2020-08-18 11:21:55 +0600

    pioneer bank finished

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Login-area
 const loginBtn = document.getElementById('login-btn');
 loginBtn.addEventListener('click', function(){
     const loginArea = document.getElementById('login-area');
@@ -5,3 +6,40 @@ loginBtn.addEventListener('click', function(){
     const transactionArea = document.getElementById('transaction-area');
     transactionArea.style.display = 'block';
 })
+
+//Deposit-heandler-btn
+const depositBtn = document.getElementById('depositBtn');
+depositBtn.addEventListener('click', function(){
+    const depositNumber = getInputNumber('depositAmount')
+
+    updateSpanText("currentBalance", depositNumber);
+    updateSpanText('currentDeposit', depositNumber);
+})
+//Withdraw btn-handler
+const withdrawBtn = document.getElementById('withdrawBtn');
+withdrawBtn.addEventListener('click', function(){
+    const withdrawNumber = getInputNumber('withdrawAmount');
+    updateSpanText('currentWithdraw', withdrawNumber);
+    updateSpanText('currentBalance', withdrawNumber * -1);
+})
+// Get-Input-Number
+function getInputNumber(id){
+    const withdrawAmount = document.getElementById(id).value;
+    const amountNumber = parseFloat(withdrawAmount);
+    if(withdrawAmount <= 0){
+        alert('Please enter positive number');
+        document.getElementById(id).value = '';
+        return 0;
+    }
+    else{
+        document.getElementById(id).value = '';
+        return amountNumber;
+    }
+}
+//update-span-text
+function updateSpanText(id, addedNumber){
+    const currentBalance = document.getElementById(id).innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+    const totalBalance = addedNumber + currentBalanceNumber;
+    document.getElementById(id).innerText = totalBalance;
+}
